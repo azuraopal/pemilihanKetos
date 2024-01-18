1802457d5c0b90ae5c2fb910471c6884278f5431
Dyzean <ashtrath@pm.me>
2024-01-19 05:57:38 +0700

    chore: style changes for NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { LogOut } from "lucide-react";
 const NavBar = () => {
   return (
     <>
-      <div className="min-w-full flex justify-between py-3 px-4 bg-slate-800">
-        <h2 className="text-2xl text-slate-200 font-bold">EV - Ketos</h2>
+      <div className="min-w-full flex justify-between items-center py-1 px-4 bg-slate-800">
+        <h2 className="text-xl text-slate-200 font-bold">EV - Ketos</h2>
         <Dropdown>
           <DropdownButton>Admin</DropdownButton>
           <DropdownContent>
