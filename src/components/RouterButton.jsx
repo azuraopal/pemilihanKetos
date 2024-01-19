58592368f4ac919acb07afe64bf9e724187ec7e6
Dyzean <ashtrath@pm.me>
2024-01-19 19:07:22 +0700

    chore: malas bikin semantic commit message

--- src/components/RouterButton.jsx
+++ src/components/RouterButton.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const RouterButton = ({ children, icon: Icon, ...props }) => {
   return (
     <Link
-      className="flex items-center gap-3 rounded-sm bg-slate-400 px-4 py-2 font-semibold text-gray-900 transition duration-100 hover:bg-slate-500"
+      className="flex items-center gap-3 rounded-sm bg-gray-400 px-4 py-2 font-semibold text-gray-900 transition duration-100 hover:bg-gray-500"
       {...props}
     >
       {Icon && <Icon />}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import RouterButton from "@/components/RouterButton"
 import LogoOSIS from "@/assets/Logo_OSIS.png"
 
-import { Gauge, Users, SquarePen, Contact } from "lucide-react"
+import { Gauge, Users, Contact } from "lucide-react"
 
 const Sidebar = () => {
   return (
     <aside className="w-[280px]">
-      <div className="flex min-h-full flex-col items-center gap-16 bg-slate-800 px-4 py-3">
+      <div className="flex min-h-full flex-col items-center gap-8 bg-slate-800 px-4 py-3">
         <div className="h-auto w-48 rounded-md bg-slate-200 p-4">
           <img className="size-full" src={LogoOSIS} alt="Logo OSIS" />
         </div>
@@ -14,7 +14,6 @@ const Sidebar = () => {
           <RouterButton to="/" icon={Gauge}>Dashboard</RouterButton>
           <RouterButton to="/data-kandidat" icon={Users}>Data Kandidat</RouterButton>
           <RouterButton to="/daftar-siswa" icon={Contact}>Daftar Siswa</RouterButton>
-          <RouterButton to="/tambah-data-siswa" icon={SquarePen}>Tambah Data Siswa</RouterButton>
         </div>
       </div>
     </aside>
