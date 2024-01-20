629e2986fbea4ca18208963a1af0ae65741ea035
Dyzean <ashtrath@pm.me>
2024-01-20 13:47:41 +0700

    style: set default font, text-color, font-size and change navbar text-color to white

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <>
       <div className="flex min-w-full items-center justify-between bg-slate-800 px-4 py-1">
-        <h2 className="text-xl font-bold text-slate-200">EV - Ketos</h2>
+        <h2 className="text-xl font-bold text-white">EV - Ketos</h2>
         <Dropdown>
           <DropdownButton>Admin</DropdownButton>
           <DropdownContent>
