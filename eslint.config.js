7d80717a1dcec8876696b7e3d4cb3f0e3c29843d
Dyzean <ashtrath@pm.me>
2024-01-21 03:33:15 +0700

    chore: update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import antfu from "@antfu/eslint-config"
+import { FlatCompat } from "@eslint/eslintrc"
+
+const compat = new FlatCompat()
+
+export default antfu(
+  {
+    react: true,
+    stylistic: {
+      indent: 2,
+      quotes: "single",
+      overrides: {
+        "multiline-ternary": "off"
+      }
+    },
+  },
+  {
+    rules: {
+      "react/prop-types": "off"
+    }
+  },
+
+  // For tailwindcss class sorter.
+  ...compat.config({
+    plugins: ["tailwindcss"],
+    extends: [
+      "plugin:tailwindcss/recommended"
+    ],
+  }),
+)
