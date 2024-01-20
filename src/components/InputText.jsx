de6f2db1015d7345dfd4bc10e5fffaad765ad89a
Dyzean <ashtrath@pm.me>
2024-01-21 01:24:42 +0700

    feat: add InputText components

---
+++ src/components/InputText.jsx
@@ -0,0 +1,8 @@
+const InputText = ({ ...props }) => (
+  <input
+    className="block w-full rounded-md border-0 px-3 py-1.5 text-sm text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600"
+    {...props}
+  />
+);
+
+export default InputText;
