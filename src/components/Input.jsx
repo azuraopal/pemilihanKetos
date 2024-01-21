1bfad6d98ba54fa9a42cefc1dc780288eb9c2d35
Dyzean <ashtrath@pm.me>
2024-01-21 16:49:54 +0700

    chore: rename InpuText component to Input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-function InputText({ ...props }) {
+function Input({ ...props }) {
   return (
     <input
       className="block w-full rounded-md border-0 px-3 py-1.5 text-sm text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600"
@@ -7,4 +7,4 @@ function InputText({ ...props }) {
   )
 }
 
-export default InputText
+export default Input
