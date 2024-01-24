3b558533caf02bae4fb3801af32fb9f25aebdb52
Dyzean <ashtrath@pm.me>
2024-01-24 16:46:18 +0700

    feat(Navbar): sample for handleLogin

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,15 @@
 import { LogOut } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 import { Dropdown, DropdownButton, DropdownContent, DropdownItem } from '@/components/Dropdown'
 
 function NavBar() {
+  const navigate = useNavigate()
+
+  function handleLogout() {
+    // Logic buat Logout
+    return navigate('/login')
+  }
+
   return (
     <>
       <div className="flex min-w-full items-center justify-between bg-slate-800 px-4 py-1">
@@ -9,7 +17,7 @@ function NavBar() {
         <Dropdown>
           <DropdownButton>Admin</DropdownButton>
           <DropdownContent>
-            <DropdownItem icon={LogOut} to="/logout">
+            <DropdownItem onClick={handleLogout} icon={LogOut}>
               Logout
             </DropdownItem>
           </DropdownContent>
